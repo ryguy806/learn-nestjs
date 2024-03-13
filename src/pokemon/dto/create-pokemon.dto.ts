b1d2be9432e12ca65ea731d2e164acd236c36590
Ryan Guelzo <ryan.t.guelzo.ctr@mail.mil>
2024-03-12 20:56:00 -0700

    Updated with all types.

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,4 +1,23 @@
 export class CreatePokemonDto {
   name: string;
-  type: 'Fire' | 'Water' | 'Grass';
+  type:
+    | 'Fire'
+    | 'Water'
+    | 'Grass'
+    | 'Rock'
+    | 'Ground'
+    | 'Poison'
+    | 'Bug'
+    | 'Ghost'
+    | 'Dragon'
+    | 'Dark'
+    | 'Steel'
+    | 'Fairy'
+    | 'Psychic'
+    | 'Fighting'
+    | 'Normal'
+    | 'Flying'
+    | 'Electric'
+    | 'Ice'
+    | 'Stellar';
 }
