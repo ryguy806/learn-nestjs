314420f55468ced67115b3bfcf2be8726d71de36
Ryan Guelzo <ryan.t.guelzo.ctr@mail.mil>
2024-03-12 21:06:16 -0700

    Updated validation.

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,8 +1,33 @@
-import { IsString } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 
 export class CreatePokemonDto {
   @IsString()
   name: string;
+
+  @IsEnum(
+    [
+      'Fire',
+      'Water',
+      'Grass',
+      'Rock',
+      'Ground',
+      'Poison',
+      'Bug',
+      'Ghost',
+      'Dragon',
+      'Dark',
+      'Steel',
+      'Fairy',
+      'Psychic',
+      'Fighting',
+      'Normal',
+      'Flying',
+      'Electric',
+      'Ice',
+      'Stellar',
+    ],
+    { message: 'Type must be a valid pokemon type' },
+  )
   type:
     | 'Fire'
     | 'Water'
