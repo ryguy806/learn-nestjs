887d53c2f581c4252bfc41781d6e5097ee2c6959
Ryan Guelzo <ryan.t.guelzo.ctr@mail.mil>
2024-03-13 09:41:19 -0700

    Setup database module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [PokemonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
